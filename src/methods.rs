003f8b1d653a8061531353e38985911cf5e38706
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-05-08 10:57:04 -0300

    update methods api

--- src/methods.rs
+++ src/methods.rs
@@ -1,23 +1,33 @@
-use crate::Particle;
+use crate::{particle::DIM, Particle};
 
 pub struct Euler;
 
 impl Euler {
-    pub fn method(particle: &mut Particle, delta_t: f64) {
-        for i in 0..2 {
-            particle.r[i] += delta_t * particle.v[i] + delta_t.powf(2.0) / 2.0 * particle.a[i];
-            particle.v[i] += delta_t * particle.a[i];
+    pub fn method(particle: &Particle, delta_t: f64) -> Vec<[f64; DIM]> {
+        let r = &particle.derivatives;
+        let mut new_r = r.clone();
+
+        for i in 0..DIM {
+            new_r[0][i] += delta_t * r[1][i] + delta_t.powf(2.0) / 2.0 * r[2][i];
+            new_r[1][i] += delta_t * r[2][i];
         }
+
+        new_r
     }
 }
 
 pub struct EulerMod;
 
 impl EulerMod {
-    pub fn method(particle: &mut Particle, delta_t: f64) {
+    pub fn method(particle: &mut Particle, delta_t: f64) -> Vec<[f64; DIM]> {
+        let r = &mut particle.derivatives;
+        let mut new_r = r.clone();
+
         for i in 0..2 {
-            particle.v[i] += delta_t * particle.a[i];
-            particle.r[i] += delta_t * particle.v[i] + delta_t.powf(2.0) / 2.0 * particle.a[i];
+            new_r[1][i] += delta_t * r[2][i];
+            new_r[0][i] += delta_t * r[1][i] + delta_t.powf(2.0) / 2.0 * r[2][i];
         }
+
+        new_r
     }
 }
--- src/particle.rs
+++ src/particle.rs
@@ -1,22 +1,20 @@
+pub const DIM: usize = 2;
+
 #[derive(Debug)]
 pub struct Particle {
     id: usize,
 
-    pub r: [f64; 2],
-    pub v: [f64; 2],
-    pub a: [f64; 2],
+    pub derivatives: Vec<[f64; DIM]>,
 
     radius: f64,
     mass: f64,
 }
 
 impl Particle {
-    pub fn new(id: usize, r: (f64, f64), v: (f64, f64), radius: f64, mass: f64) -> Self {
+    pub fn new(id: usize, r: [f64; DIM], v: [f64; DIM], radius: f64, mass: f64) -> Self {
         Self {
             id,
-            r: [r.0, r.1],
-            v: [v.0, v.1],
-            a: [0.0, 0.0],
+            derivatives: vec![r, v],
             radius,
             mass,
         }
