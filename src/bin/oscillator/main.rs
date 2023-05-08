b4bcc4208c725ac30dbc2aa7d80fbc4a4ff1b56a
Juan Negro <jnegro@itba.edu.ar>
2023-05-08 20:08:31 -0300

    add oscillator main

--- src/bin/oscillator/main.rs
+++ src/bin/oscillator/main.rs
@@ -1,6 +1,24 @@
+use constants::{acceleration_function, DIM};
+use integration_dynamics::methods::Euler;
+use simulation::OscillatorSimulation;
+use std::io::{BufWriter, Write};
+
 mod constants;
 mod simulation;
 
 fn main() {
-    println!("Hello, world!");
+    let integration_method: Euler<DIM> = Euler::new(acceleration_function);
+
+    let mut simulation = OscillatorSimulation::new(0.01, Box::new(integration_method));
+
+    let steps = simulation.run(10000);
+
+    // Write to file
+    let file = std::fs::File::create("oscillator.xyz").unwrap();
+    let mut writer = BufWriter::new(file);
+    for step in steps {
+        writeln!(writer, "1").unwrap();
+        writeln!(writer, "Properties=pos:R:1:velo:R:1",).unwrap();
+        writeln!(writer, "{:.8} {:.8}", step[0][0], step[1][0]).unwrap();
+    }
 }
