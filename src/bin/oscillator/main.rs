78c4da1654cbe62e82f77e76969156405cb0c90b
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-05-09 21:30:43 -0300

    update main

--- src/bin/oscillator/main.rs
+++ src/bin/oscillator/main.rs
@@ -1,31 +1,54 @@
-use constants::{acceleration_function, DIM};
-use integration_dynamics::methods::Euler;
-use simulation::OscillatorSimulation;
-use std::io::{BufWriter, Write};
+use anyhow::Result;
 use clap::Parser;
 
+use integration_dynamics::methods::{Euler, EulerMod, IntegrationMethod};
+
 use args::{Cli, Integration};
+use constants::{acceleration_function, analytic_solution, DIM};
+use io::{output_data, output_simulation, Data};
+use simulation::Oscillator;
+
 mod args;
 mod constants;
 mod io;
 mod simulation;
 
-fn main() {
-    let integration_method: Euler<DIM> = Euler::new(acceleration_function);
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    let mut simulation = OscillatorSimulation::new(0.0001, Box::new(integration_method));
+    let integration_method: Box<dyn IntegrationMethod<DIM>> = match args.integration_method {
+        Integration::Euler => Box::new(Euler::new(acceleration_function)),
+        Integration::EulerMod => Box::new(EulerMod::new(acceleration_function)),
+    };
 
-    let steps = simulation.run(50000);
+    let mut simulation = Oscillator::new(args.simulation_delta_t, integration_method);
 
-    // Write to file
-    let file = std::fs::File::create("oscillator.xyz").unwrap();
-    let mut writer = BufWriter::new(file);
-    for step in steps {
-        writeln!(writer, "3").unwrap();
-        writeln!(writer, "Properties=pos:R:1:velo:R:1:radius:R:1",).unwrap();
-        writeln!(writer, "{:.8} {:.8} 0.1", step[0][0], step[1][0]).unwrap();
-        writeln!(writer, "-1.5, 0.0 0.0").unwrap();
-        writeln!(writer, "1.5, 0.0 0.0").unwrap();
+    let output_iters = (args.max_time / args.output_delta_t) as usize;
+    let simulation_iters = (args.output_delta_t / args.simulation_delta_t) as usize;
+
+    let mut steps = Vec::new();
+    let mut data = Vec::new();
+
+    for i in 0..output_iters {
+        let time = i as f64 * args.output_delta_t;
+
+        let r = simulation.run(simulation_iters);
+
+        let numeric_position = r[0][0];
+        let analitic_position = analytic_solution(time);
+        let mean_square_error = (numeric_position - analitic_position).powi(2) / 2.0;
+
+        steps.push((r[0][0], r[1][0]));
+        data.push(Data::new(
+            time,
+            numeric_position,
+            analitic_position,
+            mean_square_error,
+        ));
     }
+
+    output_simulation(&args.xyz_output_path, &steps)?;
+    output_data(&args.data_output_path, &data)?;
+
+    Ok(())
 }
--- src/bin/oscillator/simulation.rs
+++ src/bin/oscillator/simulation.rs
@@ -4,13 +4,13 @@ use crate::constants::{
     DIM, INITIAL_ACCELERATION, INITIAL_POSITION, INITIAL_VELOCITY, PARTICLE_MASS,
 };
 
-pub struct OscillatorSimulation {
+pub struct Oscillator {
     pub particle: Particle<DIM>,
     pub integration_method: Box<dyn IntegrationMethod<DIM>>,
     pub delta_t: f64,
 }
 
-impl OscillatorSimulation {
+impl Oscillator {
     pub fn new(delta_t: f64, integration_method: Box<dyn IntegrationMethod<DIM>>) -> Self {
         let particle: Particle<DIM> = Particle::new(
             INITIAL_POSITION,
@@ -27,16 +27,14 @@ impl OscillatorSimulation {
         }
     }
 
-    pub fn run(&mut self, steps: usize) -> Vec<Vec<[f64; DIM]>> {
-        let mut positions = vec![];
-        positions.push(self.particle.cloned_derivatives());
+    pub fn run(&mut self, steps: usize) -> Vec<[f64; DIM]> {
         for _ in 0..steps {
             self.particle.set_derivatives(
                 self.integration_method
                     .calculate_step(&self.particle, self.delta_t),
             );
-            positions.push(self.particle.cloned_derivatives());
         }
-        positions
+
+        self.particle.cloned_derivatives()
     }
 }
